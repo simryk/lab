527d3d73ffa2a637b31e49ccd9b7c0b46a2360d8
simryk <133791987+simryk@users.noreply.github.com>
2025-04-08 18:53:34 +0300

    Add files via upload

---
+++ lab2/lab2.cpp
@@ -0,0 +1,82 @@
+﻿#include <iostream>
+#include <string>
+
+class Character {
+private:
+    std::string name;
+    int health;
+    int attack;
+    int defense;
+
+public:
+    Character(const std::string& n, int h, int a, int d)
+        : name(n), health(h), attack(a), defense(d) {
+        std::cout << "Character " << name << " created!\n";
+    }
+
+    ~Character() {
+        std::cout << "Character " << name << " destroyed!\n";
+    }
+
+    void displayInfo() const {
+        std::cout << "Name: " << name << ", HP: " << health
+            << ", Attack: " << attack << ", Defense: " << defense << std::endl;
+    }
+};
+
+class Monster {
+private:
+    std::string name;
+    int health;
+    int attack;
+    int defense;
+
+public:
+    Monster(const std::string& n, int h, int a, int d)
+        : name(n), health(h), attack(a), defense(d) {
+        std::cout << "Monster " << name << " created!\n";
+    }
+
+    ~Monster() {
+        std::cout << "Monster " << name << " destroyed!\n";
+    }
+
+    void displayInfo() const {
+        std::cout << "Name: " << name << ", HP: " << health
+            << ", Attack: " << attack << ", Defense: " << defense << std::endl;
+    }
+};
+
+class Weapon {
+private:
+    std::string name;
+    int damage;
+    float weight;
+
+public:
+    Weapon(const std::string& n, int d, float w)
+        : name(n), damage(d), weight(w) {
+        std::cout << "Weapon " << name << " created!\n";
+    }
+
+    ~Weapon() {
+        std::cout << "Weapon " << name << " destroyed!\n";
+    }
+
+    void displayInfo() const {
+        std::cout << "Weapon: " << name << ", Damage: " << damage
+            << ", Weight: " << weight << " kg" << std::endl;
+    }
+};
+
+int main() {
+    Weapon spear("Ice Death", 99999, 3);
+    Weapon knife("Kitchen knife", 15, 0.5);
+    Weapon crossbow("pickle", 0, 1);
+
+    spear.displayInfo();
+    knife.displayInfo();
+    crossbow.displayInfo();
+
+    return 0;
+}
